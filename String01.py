5e43698c3b55a86e4da5fa41032702ff92074dc2
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-11-01 15:01:46 +0300

    string 1

--- String01.py
+++ String01.py
@@ -1,6 +1,6 @@
 def main(s):
     """
-    A str of several words is given. All letters are lowercase. Make sure that the first letter of each word is capitalized.
+    A str of several  words is given. All letters are lowercase. Make sure that the first letter of each word is capitalized.
     Args:
         s: str 
     Retu rns:
